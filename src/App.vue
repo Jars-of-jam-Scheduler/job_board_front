2f413abab54caff8904621b28e2f4945049fb439
Jars <anon>
2022-12-11 10:23:16 +0100

    Add jobs list and login form

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,101 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
 
+<template>
+    <div>
+      <h1>Jf Boadrd</h1>
+testf
+        <!-- Login popup -->
+        <div id="loginPopup" v-if="showLoginPopup">
+        <h3>Login</h3>
+        <form @submit.prevent>
+          <input type="text" placeholder="Username" v-model="username" />
+          <input type="password" placeholder="Password" v-model="password" />
+          <button @click="login">Login</button>
+          <button @click="toggleLoginPopup">Cancel</button>
+        </form>
+      </div>
+
+      <button @click="toggleLoginPopup">Login</button>
+  
+      <ul>
+        <li v-for="(job, index) in jobs" :key="index">
+          <h3>{{ job.title }}</h3>
+          <p>{{ job.description }}</p>
+        </li>
+      </ul>
+      
+    </div>
+</template>
+  
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      jobs: [],
+      page: 1,
+      isLoading: false,
+      showLoginPopup: false,
+      username: '',
+      password: '',
+    }
+  },
+  mounted() {
+    this.fetchJobs();
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  beforeUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+  methods: {
+    fetchJobs() {
+      // Only fetch jobs if we're not already loading
+      if (this.isLoading) {
+        return;
+      }
+      this.isLoading = true;
+
+      axios
+        .get('http://localhost:80/api/jobs?page=' + this.page)
+        .then(response => {
+          this.jobs = [...this.jobs, ...response.data.data];
+          this.page++;
+          this.isLoading = false;
+        })
+        .catch(error => {
+          console.log(error);
+          this.isLoading = false;
+        });
+    },
+    handleScroll() {
+      const bottom =
+        document.documentElement.scrollHeight -
+        (window.innerHeight + window.scrollY);
+      if (bottom <= 250) {
+        this.fetchJobs();
+      }
+    },
+    toggleLoginPopup() {
+      this.showLoginPopup = !this.showLoginPopup;
+    },
+    login() {
+        axios
+        .post('http://localhost:80/api/sanctum/token', {
+            email: this.username,
+            password: this.password,
+            device_name: 'webapp_device'
+        })
+        .then(response => {
+            // Login was successful, save the token to use for future requests
+            localStorage.setItem('token', response.data.token);
+            this.showLoginPopup = false;
+        })
+        .catch(error => {
+            // Login failed, display an error message to the user
+            alert('Login failed, please try again');
+            console.error(error)
+        });
+  }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
